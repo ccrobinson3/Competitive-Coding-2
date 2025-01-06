8d2c6ff8bdea6d29ab61e0722a3d7279cf3c312a
Chellsea Robinson <chellseacarmel3@gmail.com>
2025-01-06 04:32:06 -0500

    Solved interview question

---
+++ Problem1.py
@@ -0,0 +1,21 @@
+###### Sum of two numbers to target values ######
+
+
+# go through the array and try to find the target - nums[i] in the array
+
+def index_of_two_numbers_to_target(nums,k):
+    if not nums:
+        return [-1,-1]
+    
+    index_map = {}
+    for i in range(len(nums)):
+        if (k-nums[i]) in index_map:
+            return [i,index_map[k-nums[i]]]
+
+        else:
+            index_map[nums[i]] = i
+            
+    return [-1,-1]
+
+
+print(index_of_two_numbers_to_target([1,2,5,6],8))
